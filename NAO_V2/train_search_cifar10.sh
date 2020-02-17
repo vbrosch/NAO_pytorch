f94254ef618c121d0295a6df30a499f01c7c3af5
Valentin Brosch <brosch@sternsinger.de>
2020-02-17 21:48:10 +0100

    make nodes and layers configurable

--- NAO_V2/train_search_cifar10.sh
+++ NAO_V2/train_search_cifar10.sh
@@ -4,4 +4,4 @@ DATA_DIR=data
 
 mkdir -p $OUTPUT_DIR
 
-python train_search.py --data=$DATA_DIR --output_dir=$OUTPUT_DIR | tee -a $OUTPUT_DIR/train.log
+python train_search.py --child_nodes $1 --child_layers $2 --data=$DATA_DIR --output_dir=$OUTPUT_DIR | tee -a $OUTPUT_DIR/train.log
