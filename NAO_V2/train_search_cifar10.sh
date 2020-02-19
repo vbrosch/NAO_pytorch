0c2f0ecfc5b9a2a6ed1fdb998d569252584182f5
Valentin Brosch <valentin.brosch@aibro.de>
2020-02-19 15:20:48 +0100

    revert param change

--- NAO_V2/train_search_cifar10.sh
+++ NAO_V2/train_search_cifar10.sh
@@ -1,7 +1,7 @@
 MODEL=search_cifar10
-OUTPUT_DIR=$3
-DATA_DIR=$4
+OUTPUT_DIR=$1
+DATA_DIR=$2
 
 mkdir -p $OUTPUT_DIR
 
-python3 train_search.py --child_nodes $1 --child_layers $2 --data=$DATA_DIR --output_dir=$OUTPUT_DIR | tee -a $OUTPUT_DIR/train.log
+python3 train_search.py --data=$DATA_DIR --output_dir=$OUTPUT_DIR | tee -a $OUTPUT_DIR/train.log
--- palma/nao_job.sh
+++ palma/nao_job.sh
@@ -3,8 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --partition=gpuv100
+#SBATCH --mem=32G
 #SBATCH --gres=gpu:1
-#SBATCH --time=12:00:00
+#SBATCH --time=14:00:00
 
 #SBATCH --job-name=NAO_Job
 #SBATCH --output=output.log
@@ -16,4 +17,4 @@ module load GCCcore
 module load Python/3.7.2
 module load fosscuda
 
-srun bash train_search_cifar10.sh 3 2 /scratch/tmp/v_bros02/nao /scratch/tmp/v_bros02/nao_data
+srun bash train_search_cifar10.sh /scratch/tmp/v_bros02/nao /scratch/tmp/v_bros02/nao_data
