8b9a9af3654cc08f65f014a05ea1fc46afbfe6ee
Valentin Brosch <valentin.brosch@aibro.de>
2020-02-19 20:36:49 +0100

    update to new search space

--- NAO_V2/train_search_cifar10.sh
+++ NAO_V2/train_search_cifar10.sh
@@ -4,4 +4,4 @@ DATA_DIR=$2
 
 mkdir -p $OUTPUT_DIR
 
-python3 train_search.py --data=$DATA_DIR --output_dir=$OUTPUT_DIR | tee -a $OUTPUT_DIR/train.log
+python3 train_search.py --child_nodes 3 --child_layers 1 --child_epochs 5 --controller_new_arch 50 --controller_seed_arch 200 --data=$DATA_DIR --output_dir=$OUTPUT_DIR | tee -a $OUTPUT_DIR/train.log
